aa731f664bb024c4daaa88079fa0d4bd21220c8c
tmichel- <tmichel-@student.42.fr>
2024-11-22 19:29:46 +0100

    initial commit

---
+++ spinning_cube.c
@@ -0,0 +1,65 @@
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+
+float A, B, C;
+
+float cubeWidth = 10;
+float width = 160, height = 44;
+float zBuffer = [160 * 44];
+int backgroundASCIICode = ' ';
+int distanceFromCam = 60;
+float K1 = 40;
+
+float incrementSpeed = 0.6;
+
+float x, y, z;
+float ooz;
+int xp, yp;
+
+float calculateX(int i, int j, int k) {
+    return j * sin(A) * sin(B) * cos(C) - k * cos(A) * sin(B) * cos(C) +
+           j * cos(A) * sin(B) * sin(C) + k * sin(A) * sin(C) + i * cos(B) * cos(C);
+}
+
+float calculateY(int i, int j, int k) {
+    return j * cos(A) * cos(C) - k * sin(A) * cos(C) -
+           j * sin(A) * sin(B) * sin(C) + k * cos(A) * sin(B) * sin(C) + i * cos(B) * cos(C);
+}
+
+float calculateZ(int i, int j, int k) {
+    return k * cos(A) * cos(B) - j * sin(A) * cos(B) + i * sin(B);
+}
+
+void calculateForSurface(float cubeX, float cubeY, float cubeZ, int ch) {
+    x = calculateX(cubeX, cubeY, cubeZ);
+    y = calculateY(cubeX, cubeY, cubeZ);
+    z = calculateZ(cubeX, cubeY, cubeZ) + distanceFromCam;
+
+    ooz = 1/z;
+    xp = (int)(width/2 + K1 * ooz * x * 2);
+    yp = (int)(height/2 + K1 * ooz * y);
+
+    idx = xp + yp * width;
+    if (idx>=0 && idx < width * height) {
+        if(ooz > zBuffer[idx]) {
+            zBuffer[idx] = oox;
+            buffer[idx] = ch;
+        }
+    }
+}
+
+
+int main (){
+    printf("\x1b[2j");
+    while(1) {
+        memset(buffer, backgroundASCIICode, width * height);
+        memset(zBuffer, 0, width * height * 4);
+        for (float cubeX = - cubeWidth; cubeX < cubeWidth; cubeX += incrementSpeed) {
+            for (float cubeY = - cubeWidth; cubeY < cubeWidth; cubeY += incrementSpeed) {
+                calculateForSurface(cubeX, cubeY, -cubeWidth, '#');
+            }
+        }   
+    }
+    return 0
+}
