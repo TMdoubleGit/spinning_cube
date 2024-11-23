2c7317211913b8ed95b67fa39eba7a9b69aad49f
tmichel- <tmichel-@student.42.fr>
2024-11-23 13:44:24 +0100

    fnl project / utility: 0 / fun: 100

--- spinning_cube.c
+++ spinning_cube.c
@@ -1,15 +1,16 @@
 #include <math.h>
 #include <stdio.h>
+#include <unistd.h>
 #include <string.h>
 
 float A, B, C;
 
-float cubeWidth = 10;
-float width = 160, height = 44;
+float cubeWidth = 20;
+int width = 160, height = 44;
 float zBuffer[160 * 44];
 char buffer[160*44];
 int backgroundASCIICode = ' ';
-int distanceFromCam = 60;
+int distanceFromCam = 100;
 float K1 = 40;
 
 float incrementSpeed = 0.6;
@@ -21,12 +22,12 @@ int idx;
 
 float calculateX(int i, int j, int k) {
     return j * sin(A) * sin(B) * cos(C) - k * cos(A) * sin(B) * cos(C) +
-           j * cos(A) * sin(B) * sin(C) + k * sin(A) * sin(C) + i * cos(B) * cos(C);
+           j * cos(A) * sin(C) + k * sin(A) * sin(C) + i * cos(B) * cos(C);
 }
 
 float calculateY(int i, int j, int k) {
-    return j * cos(A) * cos(C) - k * sin(A) * cos(C) -
-           j * sin(A) * sin(B) * sin(C) + k * cos(A) * sin(B) * sin(C) + i * cos(B) * cos(C);
+    return j * cos(A) * cos(C) + k * sin(A) * cos(C) -
+           j * sin(A) * sin(B) * sin(C) + k * cos(A) * sin(B) * sin(C) - i * cos(B) * sin(C);
 }
 
 float calculateZ(int i, int j, int k) {
@@ -57,9 +58,15 @@ int main (){
     while(1) {
         memset(buffer, backgroundASCIICode, width * height);
         memset(zBuffer, 0, width * height * 4);
-        for (float cubeX = - cubeWidth; cubeX < cubeWidth; cubeX += incrementSpeed) {
-            for (float cubeY = - cubeWidth; cubeY < cubeWidth; cubeY += incrementSpeed) {
-                calculateForSurface(cubeX, cubeY, -cubeWidth, '#');
+        for (float cubeX = -cubeWidth; cubeX < cubeWidth; cubeX += incrementSpeed) {
+            for (float cubeY = -cubeWidth; cubeY < cubeWidth; cubeY += incrementSpeed) {
+                calculateForSurface(cubeX, cubeY, -cubeWidth, '.');
+                calculateForSurface(cubeWidth, cubeY, cubeX, '$');
+                calculateForSurface(-cubeWidth, cubeY, -cubeX, '%');
+                calculateForSurface(-cubeX, cubeY, cubeWidth, '~');
+                calculateForSurface(cubeX, -cubeWidth, -cubeY, '?');
+                calculateForSurface(cubeX, cubeWidth, cubeY, 'a');
+
             }
         }
         printf("\x1b[H");
@@ -67,7 +74,8 @@ int main (){
             putchar(k % width ? buffer[k] : 10);
         }
         A += 0.005;
-        B += 0.005;  
+        B += 0.005;
+        usleep(1000);
     }
     return 0;
 }
